ceada2bc5fdccce7f3a8a4da292b6da839798bfe
李艳兴 <liyanxing@zhongruigroup.com>
2021-10-09 09:36:32 +0800

    项目创建

---
+++ src/test/java/com/wzw/AppTests.java
@@ -0,0 +1,15 @@
+package com.wzw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
