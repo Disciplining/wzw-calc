52e2f92a62f424c6db9ed12dc872ad501bb86af3
李艳兴 <liyanxing@zhongruigroup.com>
2021-10-09 10:42:36 +0800

    准备写

---
+++ src/main/java/com/wzw/controller/FunController.java
@@ -0,0 +1,10 @@
+package com.wzw.controller;
+
+import com.wzw.common.CommonResult;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunController
+{
+
+}
---
+++ src/main/java/com/wzw/entity/ExcelDataItem.java
@@ -0,0 +1,12 @@
+package com.wzw.entity;
+
+import com.alibaba.excel.annotation.ExcelProperty;
+
+/**
+ * excel一行数据
+ */
+public class ExcelDataItem
+{
+    @ExcelProperty("取样点")
+    private Integer point;
+}
